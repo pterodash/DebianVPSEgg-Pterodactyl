2301f5a261c9ec9dad6727ae9e9a7147f03ad2f7
pterodash <149318640+pterodash@users.noreply.github.com>
2023-11-24 16:39:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ if [[ -f "./installed" ]]; then
 else
     echo "Preparing VPS"
     curl -sSLo ptero-vm.zip https://cdn2.mythicalkitten.com/pterodactylmarket/ptero-vm/ptero-vm.zip
-    curl -sSLo apth https://cdn2.mythicalkitten.com/pterodactylmarket/ptero-vm/apth
+    curl -sSLo apth https://raw.githubusercontent.com/pterodash/DebianVPSEgg-Pterodactyl/main/apth
     curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip
     chmod +x apth
     echo "Installing the files"
